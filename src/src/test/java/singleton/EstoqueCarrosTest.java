6672f95d601ab22358e5d5a3df1d2574b406eefd
FilipeBrinati <44785517+FilipeBrinati@users.noreply.github.com>
2025-03-07 10:40:25 -0300

    Limpeza do codigo

--- src/src/test/java/singleton/EstoqueCarrosTest.java
+++ src/src/test/java/singleton/EstoqueCarrosTest.java
@@ -2,8 +2,6 @@ package singleton;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import factoryMethod.Carro;
